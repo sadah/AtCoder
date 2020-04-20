1a2bacda6eb96aee8c44720abe07e5206400f602
sadah <sadaaki.hirai@gmail.com>
2020-04-20 23:20:51 +0900

    added some tips

--- templates/ruby/tips.rb
+++ templates/ruby/tips.rb
@@ -1,3 +1,20 @@
+# https://qiita.com/jkr_2255/items/3c9bc8273da818a39810
+# 組み合わせの総数
+# n個から、k個を取り出す場合の総数
+#
+# https://mathtrain.jp/tyohukuc
+# 重複あり組み合わせの場合は nHk = n+k-1Ck
+def count_combi(n, k)
+  k = n - k if 2 * k > n
+  return 1 if k == 0
+  ((n - k + 1)..n).inject(&:*) / ((1..k).inject(&:*))
+end
+
+# https://qiita.com/rtoya/items/6d248b0817acd399603b
+# 階乗
+def factorial(number)
+  (1..number).inject(1,:*)
+end
 
 # 配列に出現する数値をグループ化
 # {1=>[1, 1, 1], 2=>[2], 3=>[3, 3]}
@@ -8,7 +25,6 @@ p a_list.group_by(&:itself)
 # {1=>3, 2=>1, 3=>2}
 a_list =[]
 p a_list.group_by(&:itself).map{|k,v| [k, v.count] }.to_h
-
 
 # Ruby で競技プログラミングをするときのまとめ（多次元配列編）
 # https://qiita.com/n4o847/items/afaec6629e21fd080e97
