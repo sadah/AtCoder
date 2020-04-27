8da59d49890a3bab9576912ce7f38c84ad508885
sadah <sadaaki.hirai@gmail.com>
2020-04-27 21:56:32 +0900

    updated

--- Others/dp/a/Main.rb
+++ Others/dp/a/Main.rb
@@ -7,17 +7,37 @@ end
 
 N, h_list = gets_n_and_i_list      
 
-dp = []
+# 貰うDP
+# dp = []
+# dp[0] = 0
+
+# (1...N).each do |i|
+#   s1 = (h_list[i] - h_list[i - 1]).abs + dp[i - 1]
+#   if i >= 2
+#     s2 = (h_list[i] - h_list[i - 2]).abs +  + dp[i - 2]
+#   else
+#     s2 = Float::INFINITY
+#   end
+#   dp[i] = [s1, s2].min
+# end
+
+# puts dp.last
+
+# 配るDP
+dp = Array.new(N, Float::INFINITY)
 dp[0] = 0
 
-(1...N).each do |i|
-  s1 = (h_list[i] - h_list[i - 1]).abs + dp[i - 1]
-  if i >= 2
-    s2 = (h_list[i] - h_list[i - 2]).abs +  + dp[i - 2]
-  else
-    s2 = Float::INFINITY
+(0...N).each do |i|
+  if i + 1 < N
+    s1 = dp[i] + (h_list[i] - h_list[i + 1]).abs
+    dp[i+1] = [dp[i+1], s1].min 
   end
-  dp[i] = [s1, s2].min
+  if i + 2 < N
+    s2 = dp[i] + (h_list[i] - h_list[i + 2]).abs  
+    dp[i+2] = [dp[i+2], s2].min 
+  end
 end
-
 puts dp.last
+
+
+
