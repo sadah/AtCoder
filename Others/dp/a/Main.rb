64c4416bf74c8f63597f097b6db3ebf75dfa45ba
sadah <sadaaki.hirai@gmail.com>
2020-04-27 21:29:10 +0900

    added a

--- Others/dp/a/Main.rb
+++ Others/dp/a/Main.rb
@@ -1,65 +1,23 @@
 require 'pp'
 
-# 入力値を数値で返却する
-def gets_i()
-  gets.chomp.to_i
-end
-
-# 空白区切の入力値を数値の配列で返却する
-def gets_i_list()
-  gets.chomp.split(" ").map(&:to_i)
-end
-
 # Nと空白区切の入力値を数値の配列で返却する
 def gets_n_and_i_list()
   return gets.chomp.to_i, gets.chomp.split(" ").map(&:to_i)
 end
 
-# 入力値を文字列で返却する
-def gets_s()
-  gets.chomp
-end
+N, h_list = gets_n_and_i_list      
 
-# 入力値を文字の配列で返却する
-def gets_nsp_list()
-  gets.chomp.split("")
-end
+dp = []
+dp[0] = 0
 
-# 空白区切の入力を文字列の配列で返す
-def get_s_list()
-  gets.chomp.split(" ")
-end
-
-# 空白区切の入力を数値(実数)の配列で返却する
-def gets_f_list()
-  gets.chomp.split(" ").map(&:to_f)
-end
-
-# Yes or Noを返却する
-def yes_no(bool, y = "Yes", n = "No")
-  bool ? y : n
-end
-
-# n件読み込んだ結果を数値の配列で返却する
-def gets_n_i_list
-  n = gets.chomp.to_i
-  array = []
-  n.times do
-    array.push(gets.chomp.to_i)
+(1...N).each do |i|
+  s1 = (h_list[i] - h_list[i - 1]).abs + dp[i - 1]
+  if i >= 2
+    s2 = (h_list[i] - h_list[i - 2]).abs +  + dp[i - 2]
+  else
+    s2 = Float::INFINITY
   end
-  return n, array
+  dp[i] = [s1, s2].min
 end
 
-# サイズが H, W の数値の2次元配列を返却する
-def gets_HW_int
-  h, w = gets.split.map(&:to_i)
-  board = h.times.map { gets.split.map(&:to_i) }
-  return h, w, board
-end
-
-# サイズが H, W の文字の2次元配列を返却する
-def gets_HW_char
-  h, w = gets.split.map(&:to_i)
-  board = h.times.map { gets.chomp.chars }
-  return h, w, board
-end
+puts dp.last
