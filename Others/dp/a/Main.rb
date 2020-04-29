4f9a9634e081b45a9199a4e5a163312b5c4480d8
sadah <sadaaki.hirai@gmail.com>
2020-04-29 18:59:05 +0900

    updated

--- Others/dp/a/Main.rb
+++ Others/dp/a/Main.rb
@@ -5,39 +5,25 @@ def gets_n_and_i_list()
   return gets.chomp.to_i, gets.chomp.split(" ").map(&:to_i)
 end
 
-N, h_list = gets_n_and_i_list      
-
-# 貰うDP
-# dp = []
-# dp[0] = 0
-
-# (1...N).each do |i|
-#   s1 = (h_list[i] - h_list[i - 1]).abs + dp[i - 1]
-#   if i >= 2
-#     s2 = (h_list[i] - h_list[i - 2]).abs +  + dp[i - 2]
-#   else
-#     s2 = Float::INFINITY
-#   end
-#   dp[i] = [s1, s2].min
-# end
-
-# puts dp.last
-
-# 配るDP
-dp = Array.new(N, Float::INFINITY)
-dp[0] = 0
-
-(0...N).each do |i|
-  if i + 1 < N
-    s1 = dp[i] + (h_list[i] - h_list[i + 1]).abs
-    dp[i+1] = [dp[i+1], s1].min 
-  end
-  if i + 2 < N
-    s2 = dp[i] + (h_list[i] - h_list[i + 2]).abs  
-    dp[i+2] = [dp[i+2], s2].min 
-  end
+def min(a, b)
+  return a < b ? a : b
 end
+
+def max(a, b)
+  return a > b ? a : b
+end
+
+N, h_list = gets_n_and_i_list      
+INF = Float::INFINITY
+
+dp = Array.new(N, INF)
+dp[0] = 0
+dp[1] = (h_list[1] - h_list[0]).abs
+
+1.upto(N-2) do |i|
+  n1 = (h_list[i + 1] - h_list[i]).abs + dp[i]
+  n2 = (h_list[i + 1] - h_list[i - 1]).abs + dp[i - 1]
+  dp[i + 1] = min(n1, n2)
+end
+
 puts dp.last
-
-
-
