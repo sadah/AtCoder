e354cd00877491a025bdc2e79ec92d18e227675d
sadah <sadaaki.hirai@gmail.com>
2020-06-22 19:48:49 +0900

    added

--- BeginnersSelection/abs/abc088b/main.go
+++ BeginnersSelection/abs/abc088b/main.go
@@ -7,23 +7,24 @@ import (
 
 func main() {
 	var n int
-	var a []int
 
 	fmt.Scan(&n)
+
+	aList := make([]int, n)
 
 	for i := 0; i < n; i++ {
 		var t int
 		fmt.Scan(&t)
-		a = append(a, t)
+		aList[i] = t
 	}
-	sort.Sort(sort.Reverse(sort.IntSlice(a)))
-	var m, y int
-	for i, a := range a {
+	sort.Sort(sort.Reverse(sort.IntSlice(aList)))
+	var alice, bob int
+	for i, a := range aList {
 		if i%2 == 0 {
-			m += a
+			alice += a
 		} else {
-			y += a
+			bob += a
 		}
 	}
-	fmt.Println(m - y)
+	fmt.Println(alice - bob)
 }
