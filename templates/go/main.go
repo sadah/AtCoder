482d1fdb9e1a91b95cc481465200a1a48f65ee72
sadah <sadaaki.hirai@gmail.com>
2020-06-23 21:09:49 +0900

    updated

--- templates/go/main.go
+++ templates/go/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
+func main() {
+}
 
 func int2() (int, int) {
 	var x, y int
@@ -10,5 +14,30 @@ func int2() (int, int) {
 	return x, y
 }
 
-func main() {
+func IntSlice(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&slice[i])
+	}
+	return slice
+}
+
+func SumSlice(slice []int) int {
+	total := 0
+	for i := 0; i < len(slice); i++ {
+		total += slice[i]
+	}
+	return total
+}
+
+func Min(x, y int) int {
+	if x > y {
+		return y
+	} else {
+		return x
+	}
+}
+
+func IntPow(x, pow int) int {
+	return int(math.Pow(float64(x), float64(pow)))
 }
