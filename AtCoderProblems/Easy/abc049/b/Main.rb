f164a56948f18d3bc23a2907247984eda901d334
sadah <sadaaki.hirai@gmail.com>
2020-07-25 20:19:24 +0900

    added

---
+++ AtCoderProblems/Easy/abc049/b/Main.rb
@@ -0,0 +1,16 @@
+require 'pp'
+$DEBUG ? define_method(:dpp){|e|pp e} : define_method(:dpp){|e|}
+
+# 空白区切の入力値を数値の配列で返却する
+def gets_i_list()
+  gets.chomp.split(" ").map(&:to_i)
+end
+
+
+h, w = gets_i_list
+
+h.times do
+  l = gets.chomp
+  puts l
+  puts l
+end
